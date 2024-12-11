05193eea387193e780d8b1ccb9159948bb91b3f5
felipeembersisc <felipe.embersisc@gmail.com>
2024-12-11 13:37:22 -0300

    feat: add users schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,12 +1,29 @@
 import { pgTable, text, timestamp, integer, boolean } from "drizzle-orm/pg-core"
+import { createId } from "@paralleldrive/cuid2"
 
 export const activities = pgTable("activities", {
-   id: text("id").primaryKey(),
+   id: text("id")
+      .primaryKey()
+      .$defaultFn(() => createId()),
    title: text("title").notNull(),
    description: text("description"),
    timeStart: timestamp("time_start", { withTimezone: true }).notNull(),
    timeEnd: timestamp("time_end", { withTimezone: true }).notNull(),
    hasTimeToEnd: boolean("has_time_to_end").default(false),
    status: integer("status").default(0),
-   createdAt: timestamp("created_at", { withTimezone: true }).notNull().defaultNow(),
+   createdAt: timestamp("created_at", { withTimezone: true })
+      .notNull()
+      .defaultNow(),
+})
+
+export const users = pgTable("users", {
+   id: text("id")
+      .primaryKey()
+      .$defaultFn(() => createId()),
+   name: text("name").notNull(),
+   email: text("email").notNull(),
+   active: boolean("active").default(true),
+   createdAt: timestamp("created_at", { withTimezone: true })
+      .notNull()
+      .defaultNow(),
 })
